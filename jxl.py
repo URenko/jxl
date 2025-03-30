ab61eb412a99425066ca4f06f1e9d33ab7dc4339
URenko <18209292+URenko@users.noreply.github.com>
2025-03-30 11:12:50 +0000

    add options output, _silent_failed, ignore_extension

--- jxl.py
+++ jxl.py
@@ -1,25 +1,34 @@
-import subprocess, filecmp, tempfile
+from typing import Optional
+import subprocess, filecmp, tempfile, sys
 from pathlib import Path, PurePath
 from PIL import Image
 
-def cjxl(fpath: Path, *, effect=10, brotli_effort=11):
-    if fpath.suffix.lower() != '.jpg':
+def cjxl(fpath: Path, output: Optional[Path] = None, *, effect=10, brotli_effort=11, _silent_failed=False, ignore_extension=False):
+    if not ignore_extension and fpath.suffix.lower() != '.jpg':
         return fpath
     with Image.open(fpath) as im:
         if im.format != 'JPEG':
             return fpath
-    jxl_path = fpath.with_suffix(fpath.suffix.translate(str.maketrans('pgPG', 'xlXL')))
-    subprocess.run([
+    jxl_path = fpath.with_suffix(fpath.suffix.translate(str.maketrans('pgPG', 'xlXL'))) if output is None else output
+    p = subprocess.run([
         "cjxl",
         str(fpath),
         str(jxl_path),
         '--lossless_jpeg', '1',
         '-e', str(effect),
         '--brotli_effort', str(brotli_effort)
-    ], check=True)
+    ], check=(not _silent_failed))
+    if _silent_failed and p.returncode:
+        print('The exit code of cjxl is non-zero.', file=sys.stderr)
+        if jxl_path.exists():
+            jxl_path.unlink()
+        return fpath
     with tempfile.NamedTemporaryFile(suffix='.jpg') as tmp:
         subprocess.run(["djxl", str(jxl_path), tmp.name], check=True)
-        assert filecmp.cmp(str(fpath), tmp.name)
+        if not filecmp.cmp(str(fpath), tmp.name):
+            print(f'cjxl {fpath} produced jxl file that cannot be consistently decoded by djxl!', file=sys.stderr)
+            jxl_path.unlink()
+            return fpath
     fpath.unlink()
     return jxl_path
 
